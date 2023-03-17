e86e6ca60569844f2cfdb31bb15cd857488f08aa
Jonathan Lozano <jonathan.morelo@planoly.com>
2023-03-17 10:29:16 -0500

    first and second tasks

---
+++ src/tasks/task1.js
@@ -0,0 +1,9 @@
+
+  process.stdin.on('data', data => {
+    const input = data.toString();
+    let output = '';
+    for (let i = 0; i < input.length; i++) {
+      output = input[i] + output;
+    }
+    process.stdout.write(output);
+  });
---
+++ src/tasks/task2.js
@@ -0,0 +1,30 @@
+const fs = require('fs')
+const os = require('os')
+const csvtojson = require('csvtojson');
+
+const filePath = 'src/assets/nodejs-hw1-ex1.csv';
+
+const {
+    O_TRUNC,
+  } = fs.constants;
+
+fs.exists(filePath, (e) => 
+{
+    csvtojson().fromFile(filePath)
+    .then((jsonData) => {
+        writeFile(jsonData)
+    })
+})
+
+function writeFile(jsonData) {
+    fs.open('src/assets/task1.txt','w', O_TRUNC, function( e, id ) {
+        jsonData.forEach((json) => {
+            fs.writeFile(id, JSON.stringify(json) + os.EOL, 'utf8', (err) => {
+                if (err) {
+                    console.log(err);
+                }
+            });
+        });
+        fs.close(id, console.log('the file was updated!'))
+    });
+}
